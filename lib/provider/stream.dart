0d8cdd54f39e6d69e88363e60498e072799083f4
wangzhongjie <uvdream@163.com>
2019-07-30 17:11:29 +0800

    [fix] 更改input

--- lib/provider/stream.dart
+++ lib/provider/stream.dart
@@ -2,24 +2,34 @@ import 'dart:async';
 
 class CounterBloc {
   StreamController<int> _streamController;
+
   Stream<int> _stream;
   int _count;
+  String _inputVal;
 
   CounterBloc() {
     _count = 0;
+    _inputVal = '呵呵';
     _streamController = StreamController.broadcast();
     _stream = _streamController.stream;
   }
 
   Stream<int> get stream => _stream;
   int get count => _count;
-
+  String get inputVal => _inputVal;
   addCounter() {
     _streamController.sink.add(++_count);
   }
 
   subCounter() {
     _streamController.sink.add(--_count);
+  }
+
+  inputChange(String val) {
+    print('状态管理');
+    print(val);
+    _inputVal = val;
+//    _stringController.sink.add(_inputVal);
   }
 
   dispose() {
